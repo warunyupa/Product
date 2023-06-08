bb63a43c65b6fb496da3078075adb092ade45428
warunyupa lerdseng <warunyupa.ler@student.mahidol.ac.th>
2023-06-08 11:31:54 +0700

    รวม

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,11 @@
-import {Add} from './AddComp';
 import {ShowProduct} from './ShowProductComp';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-
 
 function App() {
   return (
     <>
       <h1>PRODUCTS</h1>
-      <Add />
       <ShowProduct />
     </>
   );
